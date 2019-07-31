1953ec590128558e30fdbf3a29101d0bef003be0
Bitc <3334065667@qq.com>
2019-07-31 11:15:36 +0800

    cmt  by zhong

--- src/main/java/com/example/message/MessageApplication.java
+++ src/main/java/com/example/message/MessageApplication.java
@@ -1,16 +1,21 @@
 package com.example.message;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
-@EnableEurekaClient
+@Slf4j
+@MapperScan({"com.example.message.dao"})
+@EnableCaching
+@EnableAsync
 public class MessageApplication {
-
-
-
-
 
 
     public static void main(String[] args) {
