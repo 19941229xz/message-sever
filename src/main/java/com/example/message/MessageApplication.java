6f363c7b1a326f593ca5cf3fa646fd0af095178b
hc12345678 <1737957810@qq.com>
2019-08-01 14:26:36 +0800

    commit by xiongzh 2019.8.1 14：24

--- src/main/java/com/example/message/MessageApplication.java
+++ src/main/java/com/example/message/MessageApplication.java
@@ -1,14 +1,18 @@
 package com.example.message;
 
+import com.example.message.common.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication
 @Slf4j
 @MapperScan({"com.example.message.dao"})
@@ -16,9 +20,21 @@ import org.springframework.stereotype.Controller;
 @EnableAsync
 public class MessageApplication {
 
+//    @Autowired
+//    RedisService redisService;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(MessageApplication.class, args);
     }
+
+//    @RequestMapping("/test")
+//    public String hello(){
+//        redisService.set("test","123",1000000L);
+//        return "redis test success";
+//    }
+
+
 
 }
